7ed27ea2f327d6496a4af6dbc0f628dadcb905bd
Guilherme <bastosguilherme12@gmail.com>
2022-06-08 07:51:07 -0300

    Button Main

---
+++ app/src/main/java/com/example/finalapi/APIActivity.java
@@ -0,0 +1,14 @@
+package com.example.finalapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class APIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apiactivity);
+    }
+}
