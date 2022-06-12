f72e754745f9efce5b51ba79f49f1bb087495d3a
gabrielrecoaro <gabriel.recoaro63@gmail.com>
2022-06-11 22:38:57 -0300

    Create "Conexao"

---
+++ app/src/main/java/com/example/finalapi/Conexao.java
@@ -0,0 +1,59 @@
+package com.example.finalapi;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Conexao extends SQLiteOpenHelper {
+
+    private static final String name = "db_Studio";
+    private static final int version = 1;
+
+    public Conexao(Context context){
+        super(context, name, null, version);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+
+        db.execSQL("CREATE TABLE tbUsuario(" +
+                "IdUsuario TEXT PRIMARY KEY," +
+                "NomeUsuario TEXT NOT NULL," +
+                "UsernameUsuario TEXT NOT NULL," +
+                "ProfileUsuario BLOB," +
+                "EmailUsuario TEXT UNIQUE)");
+
+        db.execSQL("CREATE TABLE tbPersonagem("")");
+
+        db.execSQL("CREATE TABLE tbFilme("")");
+
+        db.execSQL("CREATE TABLE tbQuiz("")");
+
+        db.execSQL("CREATE TABLE tbPergunta("")");
+
+        db.execSQL("CREATE TABLE tbResposta("")");
+    }
+
+    @Override
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int NewVersion){
+
+        db.execSQL("DROP TABLE IF EXISTS tbUsuario");
+        db.execSQL("DROP TABLE IF EXISTS tbPersonagem");
+        db.execSQL("DROP TABLE IF EXISTS tbFilme");
+        db.execSQL("DROP TABLE IF EXISTS tbQuiz");
+        db.execSQL("DROP TABLE IF EXISTS tbPergunta");
+        db.execSQL("DROP TABLE IF EXISTS tbResposta");
+
+        onCreate(db);
+    }
+
+    public Cursor getNovaQuery(String sql){
+        SQLiteDatabase database = getWritableDatabase();
+        Cursor cursor = database.rawQuery(sql, null);
+        return cursor;
+    }
+
+}
